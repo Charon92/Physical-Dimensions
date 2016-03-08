31316bbccaa6a96adb0af5e0386ce1ccf31c395b
Charon92 <tpegler92@gmail.com>
2016-03-08 17:27:13 +0000

    Added files via upload

---
+++ PD 2/Physical.Dimensions.2.js
@@ -0,0 +1,11 @@
+$('.gallery').click(function() {
+	$(this).toggle('.photo-gallery');
+})
+
+$('.thumbnail').click(function() {
+	$(this).toggleClass('full-size');
+})
+
+$('.full-size').click(function() {
+	$(this).next('.thumbnail');
+})
