6721e897ef6486e49b4fbcfecb570bfcdef91cbf
Charon92 <tpegler92@gmail.com>
2016-03-09 18:00:10 +0000

    Added files via upload

---
+++ PD 2/footer.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+ <footer>
+		<ul>
+        	<li>M: Lee - 07958 385887</li>
+            <li><a href = "mailto:physicaldimensions.pt@gmail.com">Click to e-mail Lee</a></li>
+            <li><a href = "facebook.com/physicaldimensions"><img class="link-image" src="FB-f-Logo__blue_58.png" /></a></li>
+            <li><a href="https://twitter.com/PhysicalD" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @PhysicalD</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
+            <li><a href= "https://uk.linkedin.com/in/physicaldimensionspt" target="_blank"><img class="link-image" src="LinkedIn-InBug-2CRev-300x265.png" /></a></li>
+            </ul>
+	</footer
+</body>
+</html>
---
+++ PD 2/header.php
@@ -0,0 +1,16 @@
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Physical Dimensions: Home</title>
+<meta name="description" content="Physical Dimensions offers award-winning personal training, sports nutrition, adventure challenges and sports science support at affordable rates." />
+<meta name="keywords" content="personal training, fitness, weight-loss, nutrition, adventure challenges, bootcamps, Canterbury, Kent, affordable, Lee Jordan, award-winning, sports science" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://www.physicaldimensions.co.uk/" />
+<meta property="og:image" content="http://fitness.sundried.com/logos/profile/physical-dimensions-logo-canterbury--733.jpg" />
+<meta property="og:description" content="Award-winning personal training at affordable prices." />
+<link href="Primary Pages.css" rel="stylesheet" type="text/css" />
+</head>
+
+
+<body>
+</body>
+</html>
