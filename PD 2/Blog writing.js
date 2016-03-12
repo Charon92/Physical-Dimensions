90e144fe68ef6c55eba6deb140fb13b11d47b06a
Tom Pegler <tpegler92@gmail.com>
2016-03-12 13:31:22 +0000

    Added files via upload

---
+++ PD 2/Blog writing.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+//textarea size
+var textContainer, textareaSize, input;
+var autoSize = function () {
+  textareaSize.innerHTML = input.value + '\n';
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  textContainer = document.querySelector('.textarea-container');
+  textareaSize = textContainer.querySelector('.textarea-size');
+  input = textContainer.querySelector('textarea');
+  
+  autoSize();
+  input.addEventListener('input', autoSize);
+});
+
+
+$(document).ready(main);
